33fb91f87211f280f61d32688dd96a96e931c8a0
Linh3009 <109840475+Linh3009@users.noreply.github.com>
2022-09-19 21:56:47 +0700

    bai tap xay dung lop chi ghi trong java

---
+++ src/lopChiGhi/Test.java
@@ -0,0 +1,11 @@
+package lopChiGhi;
+
+public class Test {
+    public static void main(String[] args) {
+        Lop lop = new Lop();
+        System.out.println(lop.toString());
+        lop.setName("Linh");
+        lop.setClass_("C07");
+        System.out.println(lop.toString());
+    }
+}
