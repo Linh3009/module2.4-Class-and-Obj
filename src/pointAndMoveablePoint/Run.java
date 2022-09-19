d69957672272b28464e3078b1ed458f58f031d4b
Linh3009 <109840475+Linh3009@users.noreply.github.com>
2022-09-19 22:17:12 +0700

    bai tap point and moveablePoint

---
+++ src/pointAndMoveablePoint/Run.java
@@ -0,0 +1,12 @@
+package pointAndMoveablePoint;
+
+public class Run {
+    public static void main(String[] args) {
+        Point p =new Point(5, 3);
+        MoveablePoint mp = new MoveablePoint(1, 1, 3, 2);
+        System.out.println(p.toString());
+        System.out.println(mp.toString());
+        mp.move();
+        System.out.println(mp.toString());
+    }
+}
