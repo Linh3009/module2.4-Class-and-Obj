b4778422b76fa6fd8445f281f484cb1adb6e3635
Linh3009 <109840475+Linh3009@users.noreply.github.com>
2022-09-19 22:27:27 +0700

    [**Bài tập] Thiết kế và triển khai lớp Triangle

---
+++ src/triangleAndShape/Run.java
@@ -0,0 +1,8 @@
+package triangleAndShape;
+
+public class Run {
+    public static void main(String[] args) {
+        Triangle t = new Triangle(3, 4, 5);
+        System.out.println(t.toString());
+    }
+}
