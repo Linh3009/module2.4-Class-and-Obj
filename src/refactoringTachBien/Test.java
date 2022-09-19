c46834b9cf6d6bb2ecbaa8b18d035b6686598c14
Linh3009 <109840475+Linh3009@users.noreply.github.com>
2022-09-19 23:40:13 +0700

    [Thực hành] Refactoring - tách biến

---
+++ src/refactoringTachBien/Test.java
@@ -0,0 +1,10 @@
+package refactoringTachBien;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("15 is "+ FizzBuzz.fizzBuzz(15));
+        System.out.println("5 is "+ FizzBuzz.fizzBuzz(5));
+        System.out.println("9 is "+ FizzBuzz.fizzBuzz(9));
+        System.out.println("11 is "+ FizzBuzz.fizzBuzz(11));
+    }
+}
