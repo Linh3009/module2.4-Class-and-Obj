ac6cf1805efa80d5c5bf4177b42c07d0949f46c7
Linh3009 <109840475+Linh3009@users.noreply.github.com>
2022-09-19 22:12:10 +0700

    bai tap lop point 2d va lop point 3d

---
+++ src/point2Dand3D/Run.java
@@ -0,0 +1,11 @@
+package point2Dand3D;
+
+public class Run {
+    public static void main(String[] args) {
+        Point2D p2 = new  Point2D(5, 7);
+        Point3D p3 = new Point3D(3, 7, 8);
+
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+    }
+}
