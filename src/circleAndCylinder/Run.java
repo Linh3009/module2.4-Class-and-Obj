bb07c6f272f11fa70f0aae9deb16ec577ba13c54
Linh3009 <109840475+Linh3009@users.noreply.github.com>
2022-09-19 22:07:23 +0700

    bai tap lop circle and cylinder

---
+++ src/circleAndCylinder/Run.java
@@ -0,0 +1,11 @@
+package circleAndCylinder;
+
+public class Run {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5, "red");
+        System.out.println(circle.toString());
+
+        Cylinder cy = new Cylinder(5, 3, "Blue");
+        System.out.println(cy.toString());
+    }
+}
